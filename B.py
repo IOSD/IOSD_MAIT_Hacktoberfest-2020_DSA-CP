23730a055b466f15120cf7b7c7546bf64c8a40a4
Shubh Bansal <00shubhbansal@gmail.com>
2020-10-29 23:21:26 +0530

    Revert "CF 677 D3 Solutions"

--- B.py
+++
@@ -1,9 +0,0 @@
-for _ in range(int(input())) :
-    input()
-    x = str(input().split())
-    l = x.find('1')
-    r = x.rfind('1')
-    ans = 0
-    for i in range(l,r+1):
-        ans += x[i]=='0'
-    print(ans)
--- Codeforces/F.py
+++
@@ -1,7 +0,0 @@
-n,m,k = list(map(int,input().split()))
-grid = [ list(map(int,input().split())) for _ in range(n) ]
-ans = 0
-for i in range(n) :
-    jod = sum(grid[i])
-    for ps in range(jod,k-1,-1):
-        
